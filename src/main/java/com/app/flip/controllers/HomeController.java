c20e008a01cb37606310cbee0fc101625dd72b45
Anastasiya Piashko <anapiashko@gmail.com>
2021-08-19 18:20:59 +0300

    get mp3 file from controller

--- src/main/java/com/app/flip/controllers/HomeController.java
+++ src/main/java/com/app/flip/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package com.app.flip.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -18,5 +22,19 @@ public class HomeController {
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World");
         return model;
+    }
+
+    @RequestMapping(value = "/getsong")
+    public ResponseEntity<byte[]> getsong(HttpServletResponse response) {
+        try {
+            Path path = Paths.get("src","main", "resources", "records", "silly.mp3");
+            response.setContentType("audio/mp3");
+            response.setContentLength((int) Files.size(path));
+            Files.copy(path, response.getOutputStream());
+            response.flushBuffer();
+
+        } catch (Exception ignored) {
+        }
+        return null;
     }
 }
