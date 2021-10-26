6b3dacbdfb00cbc38e7f6a7c43a7564d70641ab6
Anastasiya Piashko <anapiashko@gmail.com>
2021-10-27 00:41:48 +0300

    deleted default pages

--- flip-ui/src/router/index.js
+++ flip-ui/src/router/index.js
@@ -1,30 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Topic from '../views/Topic.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/topics',
-    name: 'Topics',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Topic.vue')
+    name: 'Topic',
+    component: Topic
   },
   {
     path: '/sentences',
@@ -32,7 +16,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Sentence.vue')
+    component: () => import('../views/Sentence.vue')
   }
 ]
 
