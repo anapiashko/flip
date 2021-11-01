461bd04fc105174cce98f0f476833436d4ae88bf
anapiashko <anapiashko@gmail.com>
2021-11-02 00:52:19 +0300

    updated topic icons

--- flip-ui/src/views/Topic.vue
+++ flip-ui/src/views/Topic.vue
@@ -2,15 +2,15 @@
   <div id="app">
     <TopicCard
       :topics="[{
-                                       name: 'Health',
-                                       img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
-                                       desc: `Vocabulary set to help learn words related to health. Learn useful health and healthcare vocabulary in English, including common diseases, medical supplies and equipment and medicine names.`
-                                     },
-                                     {
-                                       name: 'Travel',
-                                       img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
-                                       desc: `The travel-related words are the most important terms to know when talking about travel or taking vacations. You'll find example sentences to help provide context for learning.`
-                                     }]"
+          name: 'Health',
+          img: 'img/icons/yoga-position.png',
+          desc: `Vocabulary set to help learn words related to health. Learn useful health and healthcare vocabulary in English, including common diseases, medical supplies and equipment and medicine names.`
+        },
+        {
+          name: 'Travel',
+          img: 'img/icons/plane.png',
+          desc: `The travel-related words are the most important terms to know when talking about travel or taking vacations. You'll find example sentences to help provide context for learning.`
+        }]"
     />
   </div>
 </template>
