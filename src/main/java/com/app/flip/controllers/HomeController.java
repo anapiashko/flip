1a75d3169ef02d4af27fa1be2f9882f8cadfcd4d
anapiashko <anapiashko@gmail.com>
2022-02-11 15:34:41 +0300

    added @CrossOrigin

--- src/main/java/com/app/flip/controllers/HomeController.java
+++ src/main/java/com/app/flip/controllers/HomeController.java
@@ -7,6 +7,7 @@ import com.app.flip.services.TextGeneratorServiceImpl;
 import com.app.flip.utils.CardTopic;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@CrossOrigin
 public class HomeController {
 
     @Autowired
--- src/main/java/com/app/flip/controllers/ProgressController.java
+++ src/main/java/com/app/flip/controllers/ProgressController.java
@@ -2,6 +2,7 @@ package com.app.flip.controllers;
 
 import com.app.flip.services.ProgressServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@CrossOrigin
 public class ProgressController {
 
     private final ProgressServiceImpl progressService;
