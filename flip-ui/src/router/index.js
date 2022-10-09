908cc65f3021a0f34c989ef860830da0ab34b76c
anapiashko <anapiashko@gmail.com>
2022-10-09 21:22:06 +0300

    added auth to ui

--- flip-ui/src/router/index.js
+++ flip-ui/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Topic from '../views/Topic.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
   {
     path: '/',
     name: 'Topic',
@@ -24,6 +29,20 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register', '/home']
+  const authRequired = !publicPages.includes(to.path)
+  const loggedIn = localStorage.getItem('user')
+
+  // trying to access a restricted page + not logged in
+  // redirect to login page
+  if (authRequired && !loggedIn) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
