64c38d47f5ef12f4eb2fd1175d9c272242d00bb8
anapiashko <anapiashko@gmail.com>
2022-10-22 19:34:18 +0300

    return back styles for App.vue

--- flip-ui/src/App.vue
+++ flip-ui/src/App.vue
@@ -82,3 +82,23 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#container {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
